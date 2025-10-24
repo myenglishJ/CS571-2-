4eb818d9fe90e0c6d565588e37a296c1fb0910a8
myenglishJ <3516989296@qq.com>
2025-10-24 17:10:24 +0800

    hw4

--- hw4/src/components/Classroom.jsx
+++ hw4/src/components/Classroom.jsx
@@ -1,25 +1,120 @@
-import { Button, Container, Form, Row } from "react-bootstrap";
+import { useState,useEffect } from "react";
+import { Button, Container, Form, Row, Col, Pagination } from "react-bootstrap";
+import Student from "./Student";
 
 const Classroom = () => {
+    const [iniData,setiniData] = useState([]);
+    const [students,setStudents] = useState([]);
+    const [major,setMajor] = useState("");
+    const [name,setName] = useState("");
+    const [interest,setInterest] = useState("");
+    // const [isPressed,setIsPressed] = useState([]);
+
+    const [page,setPage]=useState(1);
+
+    useEffect(()=>{
+        fetch("https://cs571.org/rest/s25/hw4/students",{
+            headers:{
+                    "X-CS571-ID": CS571.getBadgerId()
+            }
+        })
+        .then(res =>res.json())
+        .then(data =>{
+            setiniData(data);
+            console.log(data)
+        })
+    },[])
+    
+    useEffect(()=>{
+        setStudents(
+            iniData.filter(s=>{
+            const totalName = `${s.name.first} ${s.name.last}`
+            return totalName.toLowerCase().includes(name.trim().toLowerCase());
+        })
+        .filter(s=>{
+            return s.major.toLowerCase().includes(major.trim().toLowerCase());
+        })
+        .filter(s=>{
+            for(let i of s.interests){
+                if(i.toLowerCase().includes(interest)){
+                    return true;
+                }
+            }
+        })
+        )
+    },[iniData,name,major,interest])
+
+    function pressButton(){
+        setName("");
+        setMajor("");
+        setInterest("");
+    }
+
+    function buildPage()
+    {
+        const i = Math.ceil(students.length / 24);
+        const paginationItem = [];
+        for(let j=1;j<=i;j++)
+        {
+            paginationItem.push(<Pagination.Item
+                key={j}
+                active={page==j}
+                onClick={()=>setPage(j)}
+            >
+            {j}
+            </Pagination.Item>
+            );
+        }
+        return paginationItem
+    }
+
+    function prePage(){
+        return <Pagination.Item
+                key={0}
+                onClick={() => setPage(n => n>1 ? n-1 : n)}>
+                Previous
+                </Pagination.Item>
+    }
+
+    function nextPage(){
+        const numPages = Math.ceil(students.length / 24);
+        return <Pagination.Item
+                key={numPages}
+                onClick={ () => setPage( n => n<numPages ? n+1 : n)}>
+                Next
+                </Pagination.Item>
+    }
+
     return <div>
         <h1>Badger Book</h1>
         <p>Search for students below!</p>
         <hr />
         <Form>
             <Form.Label htmlFor="searchName">Name</Form.Label>
-            <Form.Control id="searchName"/>
+            <Form.Control id="searchName" value={name} onChange={(e)=>setName(e.target.value)}/>
             <Form.Label htmlFor="searchMajor">Major</Form.Label>
-            <Form.Control id="searchMajor"/>
+            <Form.Control id="searchMajor" value={major} onChange={(e)=>setMajor(e.target.value)}/>
             <Form.Label htmlFor="searchInterest">Interest</Form.Label>
-            <Form.Control id="searchInterest"/>
+            <Form.Control id="searchInterest" value={interest} onChange={(e)=>setInterest(e.target.value)}/>
             <br />
-            <Button variant="neutral">Reset Search</Button>
+            <Button variant="neutral" onClick={pressButton}>Reset Search</Button>
+            <p>There are {students.length} student(s) matching your search.</p>
         </Form>
+        
         <Container fluid>
             <Row>
-                { /* TODO Students go here! */ }
+                {
+                    students.slice((page-1)*24,page*24).map(s => <Col xs={12} ms={12} md={6} lg={4} xl={3} key={s.id}>
+                        <Student {...s}/>
+                    </Col>)
+                }
             </Row>
         </Container>
+        <Pagination>
+            {prePage()}
+            {buildPage()}
+            {nextPage()}
+        </Pagination>
     </div>
 
 }
--- hw4/src/components/Student.jsx
+++ hw4/src/components/Student.jsx
@@ -1,7 +1,17 @@
+
+
 const Student = (props) => {
     return <div>
         <h2>{props.name.first} {props.name.last}</h2>
-        {/* TODO Student data goes here! */}
+        <h5>{props.major}</h5>
+        <p>{props.name.first} is taking {props.numCredits} credits and is from {props.fromWisconsin?"":"NOT"}Wisconsin</p>
+        <p>They have {props.interests.length} interests including...</p>
+        <ul>
+            {props.interests.map((i,index)=>
+                <li key={props.id+index}>{i}</li>
+            )
+            }
+        </ul>
     </div>
 }
 
