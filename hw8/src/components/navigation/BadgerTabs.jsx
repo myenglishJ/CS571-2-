62850819641668f5b34ade9a6a3894a2be6f9356
myenglishJ <3516989296@qq.com>
2025-11-04 14:34:13 +0800

    hw8

--- hw8/src/components/navigation/BadgerTabs.jsx
+++ hw8/src/components/navigation/BadgerTabs.jsx
@@ -1,9 +1,16 @@
 import { Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import BadgerNewsScreen from "../screens/BadgerNewsScreen.jsx";
+import BadgerPreferencesScreen from "../screens/BadgerPreferencesScreen.jsx";
+const BadgerSocialTab = createBottomTabNavigator();
 
 function BadgerTabs(props) {
-    return <>
-        <Text style={{paddingTop: 128}}>Hello World! I should make this tabs instead.</Text>
-    </>
+    return (
+        <BadgerSocialTab.Navigator >
+            <BadgerSocialTab.Screen name="News" component={BadgerNewsScreen}/>
+            <BadgerSocialTab.Screen name="Preferences" component={BadgerPreferencesScreen}/>
+        </BadgerSocialTab.Navigator>
+    )
 }
 
 export default BadgerTabs;
--- hw8/src/components/screens/BadgerNewsScreen.jsx
+++ hw8/src/components/screens/BadgerNewsScreen.jsx
@@ -1,6 +1,8 @@
 import { Text, View } from "react-native";
 
+
 function BadgerNewsScreen(props) {
+    
     return <View>
         <Text style={{paddingTop: 128}}>When I get to Step 3, I will need to be inside a nested stack navigator!</Text>
     </View>
